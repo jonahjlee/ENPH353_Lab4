7c575bf3d743bd2834ce17c2d66cefcd145a0a11
Jonah Lee <jonahjacqueslee@gmail.com>
2026-02-05 01:45:57 -0800

    Add large UI and SIFT algorithm to complete lab 4

--- SIFT_app.py
+++ SIFT_app.py
@@ -6,11 +6,13 @@ from python_qt_binding import loadUi
 import cv2
 import sys
 
+import numpy as np
+
 class My_App(QtWidgets.QMainWindow):
 
 	def __init__(self):
 		super(My_App, self).__init__()
-		loadUi("./SIFT_app.ui", self)
+		loadUi("./SIFT_app_large.ui", self)
 
 		self._cam_id = 0
 		self._cam_fps = 10
@@ -29,6 +31,8 @@ class My_App(QtWidgets.QMainWindow):
 		self._timer.timeout.connect(self.SLOT_query_camera)
 		self._timer.setInterval(1000 / self._cam_fps)
 
+		self._template_frame = None
+
 	def SLOT_browse_button(self):
 		dlg = QtWidgets.QFileDialog()
 		dlg.setFileMode(QtWidgets.QFileDialog.ExistingFile)
@@ -38,6 +42,11 @@ class My_App(QtWidgets.QMainWindow):
 
 		pixmap = QtGui.QPixmap(self.template_path)
 		self.template_label.setPixmap(pixmap)
+
+		try:
+			self._template_frame = cv2.imread(self.template_path)
+		except Exception as e:
+			print(e)
 
 		print("Loaded template image file: " + self.template_path)
 
@@ -51,10 +60,38 @@ class My_App(QtWidgets.QMainWindow):
 		return QtGui.QPixmap.fromImage(q_img)
 
 	def SLOT_query_camera(self):
-		ret, frame = self._camera_device.read()
-		#TODO run SIFT on the captured frame
+		ret, cam_frame = self._camera_device.read()
 
-		pixmap = self.convert_cv_to_pixmap(frame)
+		# Default (no template) - show only camera feed
+		pixmap = self.convert_cv_to_pixmap(cam_frame)
+
+		if self._template_frame is not None:
+			tem_frame = self._template_frame
+
+			#-- Step 1: Detect the keypoints using SIFT Detector, compute the descriptors
+			detector = cv2.SIFT_create(nfeatures=1000)
+			keypoints1, descriptors1 = detector.detectAndCompute(cam_frame, None)
+			keypoints2, descriptors2 = detector.detectAndCompute(tem_frame, None)
+			#-- Step 2: Matching descriptor vectors with a FLANN based matcher
+			# Since SURF is a floating-point descriptor NORM_L2 is used [NOT SURF - TODO CHOOSE MEST MATCHER]
+			matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_FLANNBASED)
+			knn_matches = matcher.knnMatch(descriptors1, descriptors2, 2)
+			#-- Filter matches using the Lowe's ratio test
+			ratio_thresh = 0.5
+			good_matches = []
+			for m,n in knn_matches:
+				if m.distance < ratio_thresh * n.distance:
+					good_matches.append(m)
+			#-- Draw matches
+			img_matches = np.empty((max(cam_frame.shape[0], tem_frame.shape[0]), cam_frame.shape[1]+tem_frame.shape[1], 3), dtype=np.uint8)
+			cv2.drawMatches(cam_frame, keypoints1, tem_frame, keypoints2, good_matches, img_matches, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+			#-- Show detected matches
+			# cv2.imshow('Good Matches', img_matches)
+			# cv2.waitKey()
+			pixmap = self.convert_cv_to_pixmap(img_matches)
+		else:
+			print("Template image not selected!")
+
 		self.live_image_label.setPixmap(pixmap)
 
 	def SLOT_toggle_camera(self):
